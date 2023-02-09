86f438cc3ac25afc4c9ea7f2ac072472acefe023
Fleur41 <Muriithikings456@gmail.com>
2023-02-09 18:00:28 +0300

    Completed lab

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    if array.size == 1
+        array.join()
+    elsif array.size == 2
+        array.join(" and ")
+    else
+        array.insert(-2, "and")
+        str = array.join(", ")
+        str["and,"]= "and"
+        str
+    end
 end
